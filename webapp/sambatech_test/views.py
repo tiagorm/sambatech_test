19eccdc46208764dc6e8dd6b2bb0e34835b5cd7d
tiagorm <tiagorodrigues86@gmail.com>
2013-01-11 15:59:31 -0200

    Criei index.html, modifiquei settings.py com algumas configurações iniciais.

---
+++ webapp/sambatech_test/views.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+
+def index(request):
+    
+    return render_to_response(
+              'index.html',
+              {
+               },
+              context_instance=RequestContext(request)
+              )
