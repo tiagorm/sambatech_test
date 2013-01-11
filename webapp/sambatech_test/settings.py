874f771a4582c4e62d303ed1c9ba8d99b4d36046
tiagorm <tiagorodrigues86@gmail.com>
2013-01-11 16:01:06 -0200

    Esqueci de adicionar alguns arquivos. Aprendendo GIT ainda.

--- webapp/sambatech_test/settings.py
+++ webapp/sambatech_test/settings.py
@@ -1,10 +1,11 @@
 # Django settings for sambatech_test project.
+import os.path
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Tiago Rodrigues', 'tiagorodrigues86@gmail.com'),
 )
 
 MANAGERS = ADMINS
@@ -27,7 +28,7 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = None
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -106,6 +107,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(__file__), '../templates').replace('\\','/'),
 )
 
 INSTALLED_APPS = (
